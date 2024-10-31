18ed26811e5ed1ac3282855cfdd3f93b5d3a3fb4
Matt F <mpfulde@ucdavis.edu>
2024-10-30 20:14:03 -0700

    added speed zone visual indicator

--- Obscura/scripts/camera_controllers/speed_push_box.gd
+++ Obscura/scripts/camera_controllers/speed_push_box.gd
@@ -23,6 +23,7 @@ func _process(delta: float) -> void:
 	
 	if draw_camera_logic:
 		draw_logic()
+		draw_speed_zone()
 	
 	var tpos = target.global_position
 	var cpos = global_position
@@ -141,6 +142,44 @@ func draw_logic() -> void:
 
 	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
 	material.albedo_color = Color.BLACK
+	
+	add_child(mesh_instance)
+	mesh_instance.global_transform = Transform3D.IDENTITY
+	mesh_instance.global_position = Vector3(global_position.x, target.global_position.y, global_position.z)
+	
+	#mesh is freed after one update of _process
+	await get_tree().process_frame
+	mesh_instance.queue_free()
+
+func draw_speed_zone() -> void:
+	var mesh_instance := MeshInstance3D.new()
+	var immediate_mesh := ImmediateMesh.new()
+	var material := ORMMaterial3D.new()
+	
+	mesh_instance.mesh = immediate_mesh
+	mesh_instance.cast_shadow = GeometryInstance3D.SHADOW_CASTING_SETTING_OFF
+	
+	var left:float = -speedup_zone_top_left.x
+	var right:float = speedup_zone_bottom_right.x
+	var top:float = -speedup_zone_top_left.y
+	var bottom:float = speedup_zone_bottom_right.y
+	
+	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom))
+	
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, bottom))
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom))
+	
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, bottom))
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
+	
+	immediate_mesh.surface_add_vertex(Vector3(left, 0, top))
+	immediate_mesh.surface_add_vertex(Vector3(right, 0, top))
+	immediate_mesh.surface_end()
+
+	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
+	material.albedo_color = Color.WEB_GRAY
 	
 	add_child(mesh_instance)
 	mesh_instance.global_transform = Transform3D.IDENTITY
